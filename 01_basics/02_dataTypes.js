18ec04ad6cd420c7d16ddfc0c908b5d36cb8531e
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2024-12-28 16:14:54 +0000

    commiting

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are usimg node js not browser
+
+// we can avoid semicolon
+
+let name="sarthak"
+let age=18
+let isLoggedIn=false
+let state=
+
+//number=> 2 to power 53
+//bigint
+//string => ""
+//boolean=> true/false
+//null => standalone value
+//undefined => vcalue assign hi nahi hui hai
+// symbol => for uniqueness
+
+//object
+
+console.log(typeof "sarthak");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
