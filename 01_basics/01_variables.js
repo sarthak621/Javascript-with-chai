b64752a61f0b23a0a1a5793eb64b407fe49362c9
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2024-12-28 16:02:39 +0000

    simple commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId= 144533 //cant be change
+let accountEmail= "krishnalove@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=2;   cant do bcoz const cant be change
+console.log(accountId);
+
+accountEmail="jaishreeRam@gmail.com";
+accountPassword="621"
+accountCity="delhi"
+
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+
+
