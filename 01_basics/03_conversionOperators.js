845b0064644eb9109a5ff091ec8ce5ea37f6d71a
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2024-12-28 16:26:57 +0000

    commiting

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,22 @@
+// let score=33
+let score="33abc"
+
+// console.log(typeof score);
+
+let valueinNumber=Number(score)  // it guarantees that the no. is in score
+
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber); // gives o/p not a no. NaN
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLogin=1
+let booleanIsLogin=Boolean(isLogin)
+console.log(booleanIsLogin);
+
+//1=>true; 0=>false
+//""=>false
+//"sarthak"=>true
