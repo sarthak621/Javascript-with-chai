7e85716f2498aa964e5fb55424dbacbc8b8bfe58
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2024-12-29 17:51:01 +0000

    nums and math

---
+++ 01_basics/06NumsAndMath.js
@@ -0,0 +1,42 @@
+const score=400
+// console.log(score);
+
+const balance= new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber=123.838838
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds=100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//            MATHS
+
+// console.log(Math);
+// console.log(Math.abs(-4));  //-ve to +ve
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,3,2,5,1));
+
+
+// console.log(Math.random());
+// console.log(Math.random());
+
+// console.log(Math.floor(Math.random()*10) +1);  //guarantees value lies between 0 AND 1 
+
+
+const min=10;
+const max=20
+
+console.log(Math.floor(Math.random()* (max-min+1)+min) )
+
+//no. always come between 10 and 20 
+
+
+
+
