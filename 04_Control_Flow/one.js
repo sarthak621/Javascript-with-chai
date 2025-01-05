8d911c75915eceeb638ba5e219cbdb6695169bba
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-01-05 17:04:39 +0000

    if-else

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,53 @@
+//if
+
+// const isUserLoggedIn=true
+// // if(2=="2"){
+// //    console.log("exxecute");
+   
+// // }
+
+// if(2==="2"){
+//     console.log("execute");
+//  }
+
+// const temp=41
+
+// if(temp<100){
+//     console.log("less than 100");
+    
+// }
+
+// else{
+//     console.log("greater than 100");
+    
+// }
+
+
+// //another way
+// const balance=100
+
+// if(balance>500) console.log("tested");
+// else console.log("plzz recharge");
+
+// //nested if
+// const balance =1000
+// if(balance<500){
+//      console.log("less than 500");
+// } else if(balance<750){
+//     console.log("greater than 750");
+// } else if(balance<900){
+//     console.log("greater than 900");
+// }
+// else{
+//     console.log("greater than 1200");
+
+// }
+
+
+
+const userLoggedIn=true
+const debitCard=true
+if(userLoggedIn && debitCard){
+    console.log("allow to buy course");
+    
+}
