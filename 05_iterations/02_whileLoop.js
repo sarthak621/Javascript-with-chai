518bd9ef04e05fcf0abaaa16625386c1137a8307
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-01-06 06:00:02 +0000

    while loop

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,36 @@
+ //   //    WHILE LOOP AND DO WHILE LOOP
+
+//  while (condition) {
+    
+//  }
+
+// let index=0;
+// while(index<=10){
+//     console.log(index);
+//     index++
+// }
+
+// let myArr=["batman","shaktimaan","spider"]
+// let arr=0
+// while(arr<myArr.length){
+//     console.log(myArr[arr]);
+//     arr++;
+// }
+
+
+// do while
+// do {
+    
+// } while (condition);
+
+// let score=1
+// do{
+//     console.log(`score is ${score}`);
+//     score++
+// }while(score<=10);
+
+// let score=11 //is case me ek bar loop chalega hi
+// do{
+//     console.log(`score is ${score}`);
+//     score++
+// }while(score<=10);
