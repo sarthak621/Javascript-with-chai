03e6cd9c693b13e3bb9d32fc78485884691ed515
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-01-12 13:23:23 +0000

    promises in js

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -98,3 +98,27 @@ async function consumePromiseFive(){
 }
 
 consumePromiseFive()
+
+
+// async function getAllUser(){
+//     try {
+//         const response=await fetch(`https://jsonplaceholder.typicode.com/users`)
+//         const data=await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E:",error);
+        
+//     }
+    
+// }
+
+// getAllUser()
+
+fetch(`https://jsonplaceholder.typicode.com/users`)
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>console.log(error))
